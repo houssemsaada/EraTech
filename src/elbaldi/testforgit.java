4bc425d199389febd90642cbe936cd95d0d9b39c
houssemsaada <houssem.saada37@gmail.com>
2023-02-12 17:26:04 +0100

    test commit

---
+++ src/elbaldi/testforgit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elbaldi;
+
+/**
+ *
+ * @author houss
+ */
+public class testforgit {
+
+    String name;
+
+    public testforgit(String name) {
+        this.name = name;
+    }
+
+}
